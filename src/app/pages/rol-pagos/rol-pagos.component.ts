0b2c98902b6386781e455d676f3c3ecc95f7d31a
Mateo Barzallo <mbarzallob@hotmail.com>
2023-05-09 15:19:14 -0500

    Rol

--- src/app/pages/rol-pagos/rol-pagos.component.ts
+++ src/app/pages/rol-pagos/rol-pagos.component.ts
@@ -8,24 +8,14 @@ import { Persona, PersonaService } from '../../SERVICES/persona.service';
   styleUrls: ['./rol-pagos.component.css']
 })
 export class RolPagosComponent implements OnInit{
-  ListaPersonas: Persona[] =[];
   constructor(private PersonaService: PersonaService) { 
    
   }
   ngOnInit(): void {
 
     
-    this.listarPersonas();
     
   }
 
-  listarPersonas() {
-    this.PersonaService.getPersonas().subscribe(
-      res => {
-        console.log(res)
-        this.ListaPersonas=<any>res;
-      },
-      err => console.log(err)
-    )
-  }
+  
  }
